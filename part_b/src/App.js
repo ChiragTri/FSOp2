75f6aba7dc89946b18aa9836e13ecfbc504b47b4
Chirag <chiragtrivedi17@hotmail.com>
2022-11-17 20:54:51 -0500

    2.8

--- part_b/src/App.js
+++ part_b/src/App.js
@@ -1,38 +1,60 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([ {name:'Arto Hellas'} ]) 
-  const [newName, setNewName] = useState("")
+  const [persons, setPersons] = useState([
+    {name:'Arto Hellas', number:'123-456-7899'}
+  ]) 
+  const [newInfo, setNewInfo] = useState({
+    name:"", number:""
+  })
 
-  const addName = (event) => {
+  const addInfo = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
     // check to see if person already exists in phonebook
-    const found = persons.find(x => x.name === newName)
+    const found = persons.find(x => x.name === newInfo.name)
     if (typeof(found) !== "undefined"){
-      window.alert(`${newName} is already added to phonebook`)
+      window.alert(`${newInfo.name} is already added to phonebook`)
     }
     else{
       const newPerson = {
-        name: newName
+        name: newInfo.name,
+        number: newInfo.number
       }
     
       setPersons(persons.concat(newPerson))
-      setNewName('')
+      setNewInfo({
+        name:'', number:''
+      })
       console.log("persons", persons.concat(newPerson))
     }
   }
 
   const handleInputChange = (event) => {
     console.log(event.target.value)
-    setNewName(event.target.value)
+
+    var newName = newInfo.name
+    var newNumber = newInfo.number
+
+    if (event.target.id === "name"){
+      newName = event.target.value
+    }
+    else {
+      newNumber = event.target.value
+    }
+
+    const newText = {
+      name: newName, number: newNumber
+    }
+
+    setNewInfo(newText)
   }
 
-  const ShowName = ({ person }) => {
+  const ShowInfo = ({ person }) => {
     return(
       <>
-        {person.name} <br></br>
+        {person.name} {person.number} <br></br>
       </>
       
     )
@@ -40,24 +62,15 @@ const App = () => {
 
   return (
     <div>
-      <h2>
-        Phonebook
-      </h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleInputChange} placeholder="Enter new name" />
-        </div>
-        <div>
-          <button type="submit">
-            add
-          </button>
-        </div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addInfo}>
+        <div>name: <input id="name" value={newInfo.name} onChange={handleInputChange} placeholder="Enter new name" /></div>
+        <div>number: <input id="number" value={newInfo.number} onChange={handleInputChange} placeholder="Enter number" /></div>
+        <div><button type="submit">add</button></div>
       </form>
-      <h2>
-        Numbers
-      </h2>
+      <h2>Numbers</h2>
       {persons.map(x => 
-        <ShowName key={x.name} person={x} />  
+        <ShowInfo key={x.name} person={x} />  
       )}
     </div>
   )
